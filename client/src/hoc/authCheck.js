c1012179b8d72c286182f83407bc4e6371f4e824
99-zziy <wldud8229@gmail.com>
2021-04-15 19:55:11 +0900

    [FIX] authCheck.js comment

--- client/src/hoc/authCheck.js
+++ client/src/hoc/authCheck.js
@@ -18,10 +18,10 @@ export function withAuthCheck(Component, option) {
 
             const response = await dispatch(auth())
 
-            //로그인 하지않은 상태 + 로그인 안한 유저 -> 로그인 페이지
+            //로그인 하지않은 상태인데 로그인 한 유저만 출입이 가능한 페이지이면 로그인 페이지
             if ((!response.payload.isAuth) && (option)) return props.history.push('/login')
 
-            //로그인 한 상태 + 로그인 한 유저 -> 랜딩페이지
+            //로그인 한 상태이고 로그인 한 유저는 출입불가 한 페이지이면 랜딩페이지
             if ((response.payload.isAuth) && (!option)) return props.history.push('/')
 
         }, []) //처음에만 호출
